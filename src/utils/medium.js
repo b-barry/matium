ea7508d180b71619599ade2fa9ec2dc4866435f9
Boubacar Barry <bouba@hackages.io>
2017-09-18 14:16:50 +0200

    Add createPostOptions and createPost

--- src/utils/medium.js
+++ src/utils/medium.js
@@ -1,6 +1,10 @@
 import medium from 'medium-sdk';
+import Promise from 'bluebird';
+import {error} from './output.format';
 
-export const getMediumClient = (token) => {
+Promise.promisifyAll(medium);
+
+export const getClient = (token) => {
   const client = new medium.MediumClient({
     clientId: token,
     clientSecret: token
@@ -9,4 +13,47 @@ export const getMediumClient = (token) => {
   client.setAccessToken(token);
 
   return client;
+};
+
+export const createPostOptions = (userId, {content, options}) => {
+  return Object.assign({},
+    {userId},
+    {content},
+    {options},
+    {
+      contentFormat: 'markdown',
+      publishStatus: 'draft'
+    }
+  );
+};
+
+export const createPost = async (client, data) => {
+  const {options, userId} = data;
+
+  try {
+    if (options.publication) {
+      const publications = await  client.getPublicationsForUserAsync({userId});
+
+      const pub = publications.find(({name}) => {
+        return name.toLowerCase() === options.publication.toLowerCase();
+      });
+
+      if (!pub) {
+        throw new Error('No publication by that name!')
+      }
+      return await client.createPostInPublicationAsync(Object.assign(data, {publicationId: pub.id}));
+    }
+    return await client.createPostAsync(data);
+  }
+  catch (err) {
+    console.error(
+      error(
+        `An unexpected error occurred while creating post : 
+        ${err.message}
+        \n `
+      )
+    );
+  }
+  // TODO: check if empty
+  return {};
 };
